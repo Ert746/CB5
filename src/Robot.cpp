0b1e9d13af92737156c6cc82271a7f0010b33965
Elijah <elijahmtcastle@gmail.com>
2017-01-13 21:46:42 -0500

    Camera test branch
    Tests the new camera servers out in code compiles FINE!

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,17 +1,49 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <thread>
 
-#include <WPILib.h>
+#include <WPILib.h>// might consider not including full wpilib
 
+#include <CameraServer.h>
 #include <IterativeRobot.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/types.hpp>
 
 #include <LiveWindow/LiveWindow.h>
 #include <SmartDashboard/SendableChooser.h>
 #include <SmartDashboard/SmartDashboard.h>
 
 class Robot: public frc::IterativeRobot {
+	static void VisionThread(){// multithreading is required for the image proccessing so yah
+			 cs::UsbCamera camera = CameraServer::GetInstance()->StartAutomaticCapture();//starts capturing basic images into camera
+
+			 camera.SetExposureAuto();
+			 camera.SetResolution(640,480);
+
+			 cs::CvSink sinker = CameraServer::GetInstance()->GetVideo();//Grabs video to sink into the mat image cruncher
+
+			 cs::CvSource cheese =CameraServer::GetInstance()->PutVideo("Rectangle",640,480);//Serves up the images gathered your on camera
+
+			 cv::Mat cruncher;//this is the magic image cruncher right here it eats up the memory to so don't make any more
+
+		 while(true){//image processing happens in here
+
+				 if(sinker.GrabFrame(cruncher)==0){// if theres nothing there you got problems
+
+					 cheese.NotifyError(sinker.GetError());//HEY LISTEN! you got some problems tell me about them
+					 continue;//restarts the thread I think
+			 }
+				 rectangle(cruncher, cv::Point(100, 100), cv::Point(400, 400),
+				 					cv::Scalar(255, 255, 255), 5);//draw some rectangles on that thing WOOT RECTANGLES
+
+				 			cheese.PutFrame(cruncher);//finally put the final modified frame
+
+		 }
+	}
 public:
+
 	double Leftgo,Rightgo;
 
 
@@ -27,7 +59,10 @@ public:
 	RobotDrive *robotDrive =new RobotDrive(fLeft,fRight,bLeft,bRight);
 
 	void RobotInit() {
-		chooser.AddDefault(autoNameDefault, autoNameDefault);
+		std::thread camthread(VisionThread);//makes a new thread
+		camthread.detach();//snaps the thread off to do its own thing
+
+		chooser.AddDefault(autoNameDefault, autoNameDefault);//I don't like this look into making it logical
 		chooser.AddObject(autoNameCustom, autoNameCustom);
 		frc::SmartDashboard::PutData("Auto Modes", &chooser);
 
